80191aef2f17f15c41e83f3d0503f324a313f06c
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2021-07-18 17:45:18 -0700

    implement react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/index.css';
+import SearchForm from './components/SearchForm';
+import Nav from './components/Nav';
+import PhotoContainer from './components/PhotoContainer';
+import ApiKey from './config';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+      <SearchForm />
+
+      <Nav />
+
+      <PhotoContainer />
+
+      <h1>{ApiKey}</h1>
+
     </div>
   );
 }
