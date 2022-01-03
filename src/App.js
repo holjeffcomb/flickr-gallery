6b9ed08b0bd297782d7089a8fab66f92265d9171
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2022-01-02 19:15:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from "./components/Nav";
 import PhotoContainer from "./components/PhotoContainer";
 import NotFound from "./components/NotFound";
 import { cats, dogs, computers } from "./components/NavData";
-import ApiKey from "./config";
+// import ApiKey from "./config";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
@@ -33,7 +33,7 @@ class App extends Component {
   search = (query) => {
     this.setState({ isLoading: true });
     fetch(
-      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${ApiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e6016b13c20029b297b76bc3c1ecd9b2&tags=${query}&per_page=24&format=json&nojsoncallback=1`
     )
       .then((response) => response.json())
       .then((responseData) => {
